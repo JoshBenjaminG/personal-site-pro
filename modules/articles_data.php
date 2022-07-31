4b99c5682f270b9ffdc7626a5670d44aae616645
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-07-31 18:12:25 -0500

    add project images and article data

--- modules/articles_data.php
+++ modules/articles_data.php
@@ -4,19 +4,19 @@ $database = [ // an associative array (Array of key:value pairs)
 	[
 		"heading" => "Croatian Coasts",
 		"description" => "A case study on a PHP themeing research project based on a high-end resort chain.",
-		"thumbnail" => "https://peprojects.dev/images/landscape.jpg",
+		"thumbnail" => "images/projectimage.jpg",
 		"url" => "https://peprojects.dev/alpha-3/josh/projects/layout-theme-challenge/",
 	],
 	[
 		"heading" => "Riche Fashion",
 		"description" => "A research project that implemented design principles commonly found among designer fashion e-commerce websites.",
-		"thumbnail" => "https://peprojects.dev/images/landscape.jpg",
+		"thumbnail" => "images/projectimage2.jpg",
 		"url" => "https://peprojects.dev/alpha-3/josh/projects/riche-fashion/",
 	],
 	[
 		"heading" => "Coding Exercises",
 		"description" => "Some solutions to a nifty book full of language-agnostic challenges designed to develop your coding skills.",
-		"thumbnail" => "https://peprojects.dev/images/landscape.jpg",
+		"thumbnail" => "images/projectimage3.jpg",
 		"url" => "https://peprojects.dev/alpha-3/josh/projects/e4p/",
 	],
 ];
