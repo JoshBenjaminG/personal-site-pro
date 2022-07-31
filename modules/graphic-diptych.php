619297f1f7e5308f4d8a9e145573ca48cf225c97
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-07-30 22:00:59 -0500

    add landing logo svg

--- modules/graphic-diptych.php
+++ modules/graphic-diptych.php
@@ -1,10 +1,14 @@
 
 <graphic-diptych>
+	<landing-logo>
+		<?php include('icons/landing.svg'); ?>
+	</landing-logo>
 
-	<text-content>
+<!-- 	<text-content>
+
 		<span class="greetings">Greetings,</span>
 		<span class="josh">I'm Josh!</span>
 		<span class="developer">- Web Developer</span>
-	</text-content>
+	</text-content> -->
 
 </graphic-diptych>
