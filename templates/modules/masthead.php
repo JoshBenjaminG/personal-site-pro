f5e94c4be00e02a15eacf48777b8f697d424d9c2
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-08-15 16:49:44 -0500

    add riche-fashion image folder and case study modules

--- templates/modules/masthead.php
+++ templates/modules/masthead.php
@@ -11,7 +11,7 @@
 
 	<nav class='links'>
 		<!-- <a href='?page=home' class="hover-underline-animation calm-voice">Home</a> -->
-		<a href='#projects' class="hover-underline-animation calm-voice projects-link">Projects</a>
+		<a href='?page=home#projects' class="hover-underline-animation calm-voice projects-link">Projects</a>
 		<a href="https://github.com/JoshBenjaminG" class="hover-underline-animation calm-voice">Github</a>
 		<a href="https://resume.creddle.io/resume/j9rrzc7had4" class="hover-underline-animation calm-voice">Resume</a>
 		<a href='?page=style-guide' class="hover-underline-animation calm-voice">Style Guide</a>
