552a0aaf9289b69bd5282664f8b91a36e37be9f1
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-08-25 22:42:24 -0500

    add landing section and style

---
+++ templates/modules/landing-area.php
@@ -0,0 +1,28 @@
+<?php 
+
+$header = $module["header"];
+$content = $module["content"];
+
+?>
+
+<section class='stuff'>
+<inner-column>
+
+
+  <landing-area id='projects'>
+
+
+    <project-grid>
+    
+			<?=$header?>
+
+			<div></div>
+
+			<?=$content?>
+
+    </project-grid>
+
+  </landing-area>
+
+</inner-column>
+</section>
