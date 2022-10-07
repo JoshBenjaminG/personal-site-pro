d12fdfcb02c0e02fed64ff331b17afe34772a526
JoshuaEgage <93669172+JoshuaEgage@users.noreply.github.com>
2022-10-06 21:08:02 -0500

    added images and figma case study

--- templates/modules/head.php
+++ templates/modules/head.php
@@ -3,8 +3,8 @@
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
 	<title>Joshua G | Front-end Developer</title>
-	<meta name='description' content='[[insert description]]'>
-	<meta property='og:image' content='images/default.png'>
+	<meta name='description' content='Front-End Developer'>
+	<meta property='og:image' content='images/project-image2.jpg'>
 
 	<link rel='stylesheet' href='css/style.css'>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
